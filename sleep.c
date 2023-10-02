59312901e4075f230a298ac17438f246c82e7a6f
Jana Kampova <xkampova@stuba.sk>
2023-10-02 18:45:53 +0200

    First commit

---
+++ sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+if(argc<2){
+    fprintf(2,"Usage: sleep for a new sec.../n");
+    exit(-1);
+    }
+    sleep(atoi(argv[1]));
+    exit(1);
+}
