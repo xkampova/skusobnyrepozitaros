2e7d19c944ed99893808f6626121e3da49674f3c
Jana Kampova <xkampova@stuba.sk>
2023-11-04 18:21:35 +0100

    UGETPID

--- kernel/proc.c
+++ kernel/proc.c
@@ -131,7 +131,13 @@ found:
     release(&p->lock);
     return 0;
   }
-
+  // Allocate a usyscall usscl.
+  if((p->usscl = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usscl->pid= p->pid; // doplnene referencia na tu stranku
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -158,6 +164,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+ if(p->usscl)
+    kfree((void*)p->usscl);
+  p->usscl = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -201,6 +210,13 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+// pridane
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->usscl), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
 
   return pagetable;
 }
@@ -211,6 +227,7 @@ void
 proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);//pridane
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
   uvmfree(pagetable, sz);
 }
